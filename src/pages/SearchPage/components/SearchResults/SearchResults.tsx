1dbacb1ff04f7d19cc108ef6c790b9091f1bb300
Erne1984 <ernestofamorim@gmail.com>
2024-10-01 01:51:20 -0300

    feat: pagination

--- src/pages/SearchPage/components/SearchResults/SearchResults.tsx
+++ src/pages/SearchPage/components/SearchResults/SearchResults.tsx
@@ -1,10 +1,12 @@
 import style from "./SearchResults.module.css";
 import Book from "../../../../interfaces/Book";
-import SearchResultRow from "../SearchResultsRow/SearchResultRow";
 import { useState } from "react";
 
+import SearchResultRow from "../SearchResultsRow/SearchResultRow";
 import GridView from "./components/GridView/GridView";
 import ListView from "./components/ListView/ListView";
+
+import Pagination from "./components/Pagination/Pagination";
 
 interface SearchResultsProps {
     books: Book[]
@@ -12,18 +14,31 @@ interface SearchResultsProps {
 
 export default function SearchResults(props: SearchResultsProps) {
     const [viewMode, setViewMode] = useState<string>("grid");
+    const [currentPage, setCurrentPage] = useState<number>(1); 
+    const booksPerPage = 5;
+
+    const startIndex = (currentPage - 1) * booksPerPage;
+    const endIndex = startIndex + booksPerPage;
+    const currentBooks = props.books.slice(startIndex, endIndex); 
 
     return (
         <>
             <SearchResultRow numberResults={props.books.length} viewMode={viewMode} setViewMode={setViewMode} />
 
             <div className={style["container"]}>
-                
                 {
-                    viewMode == "grid" ? <GridView books={props.books}/> : <ListView books={props.books}/>
+                    viewMode == "grid"
+                        ? <GridView books={currentBooks} /> 
+                        : <ListView books={currentBooks} />
                 }
-
             </div>
+
+            <Pagination
+                totalBooks={props.books.length}
+                booksPerPage={booksPerPage}
+                currentPage={currentPage}
+                onPageChange={setCurrentPage}
+            />
         </>
     )
 }
--- src/pages/SearchPage/components/SearchResults/components/Pagination/Pagination.tsx
+++ src/pages/SearchPage/components/SearchResults/components/Pagination/Pagination.tsx
@@ -1,1 +1,58 @@
-/* TO IMPLEMETING YET */
+import style from "./Pagination.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+interface PaginationProps {
+    totalBooks: number;
+    booksPerPage: number;
+    currentPage: number;
+    onPageChange: (page: number) => void;
+}
+
+export default function Pagination({ totalBooks, booksPerPage, currentPage, onPageChange }: PaginationProps) {
+    
+    const totalPages = Math.ceil(totalBooks / booksPerPage);
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            onPageChange(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            onPageChange(currentPage + 1);
+        }
+    };
+
+    return (
+        <div className={style["container"]}>
+            <FontAwesomeIcon
+                icon={faChevronLeft}
+                className={style["icon"]}
+                onClick={handlePreviousPage}
+                style={{ cursor: currentPage > 1 ? "pointer" : "not-allowed" }}
+            />
+
+            <div className={style["indexes"]}>
+                {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+                    <span
+                        key={page}
+                        className={`${style["index"]} ${page === currentPage ? style["active"] : ""}`}
+                        onClick={() => onPageChange(page)}
+                        style={{ cursor: "pointer" }}
+                    >
+                        {page}
+                    </span>
+                ))}
+            </div>
+
+            <FontAwesomeIcon
+                icon={faChevronRight}
+                className={style["icon"]}
+                onClick={handleNextPage}
+                style={{ cursor: currentPage < totalPages ? "pointer" : "not-allowed" }}
+            />
+        </div>
+    );
+}
